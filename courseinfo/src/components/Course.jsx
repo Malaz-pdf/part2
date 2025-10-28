633982f6ef5946a39eb28bd5ed2a7ba7a6e98f9b
Malaz Natouf <malaznatouf@gmail.com>
2025-10-28 20:39:11 +0100

    courseinfo

--- courseinfo/src/components/Course.jsx
+++ courseinfo/src/components/Course.jsx
@@ -1,7 +1,37 @@
-import Header from './Header'
-import Content from './Content'
-import Total from './Total'
 
+const Header = ({course}) => {
+  return(
+    <div>
+      <h1>{course.name}</h1>
+    </div>
+  )
+}
+const Part =({part}) => {
+  return(
+    <div>
+      <p>{part.name} {part.exercises} </p>
+    </div>
+  )
+}
+
+const Content = ({parts}) =>{
+  return (
+    <div>
+      {parts.map(part =>
+        <Part key={part.id} part={part} />
+      )}
+    </div>
+  )
+}
+
+const Total = ({parts}) => {
+  const total = parts.reduce((sum,part) => sum + part.exercises,0)
+  return(
+    <div>
+      <p> total of {total} exercises </p>
+    </div>
+  )
+}
 
 const Course = ({course}) => {
   return(
--- courseinfo/src/components/Total.jsx
+++
@@ -1,9 +0,0 @@
-const Total = ({parts}) => {
-  const total = parts.reduce((sum,part) => sum + part.exercises,0)
-  return(
-    <div>
-      <p> total of {total} exercises </p>
-    </div>
-  )
-}
-export default Total
