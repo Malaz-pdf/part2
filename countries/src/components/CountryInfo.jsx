53ae789c7a89754950ea7d0d6ee84d2e841c5e37
Malaz Natouf <malaznatouf@gmail.com>
2025-11-12 15:24:19 +0100

    all done

--- countries/src/components/CountryInfo.jsx
+++ countries/src/components/CountryInfo.jsx
@@ -1,6 +1,25 @@
+import { useState, useEffect } from "react"
+import weatherService from '/Users/malaznatouf/Desktop/Fullstack sub/part2/countries/src/services/weather.js'
+
 const CountryInfo = ({ country }) => {
 
     const languages = country.languages ? Object.values(country.languages) : []
+
+    const [weather, setWeather] = useState(null)
+
+    const capital = country.capital[0]
+
+    useEffect(() => {
+        if (!capital) return
+
+        weatherService 
+                        .getWeather(capital)
+                        .then(initialWeather =>{setWeather(initialWeather)})
+                        .catch((error) => {
+                        console.error("Error fetching weather:", error);
+                        setWeather(null);
+                         })
+    },[capital])
 
     return (
         <div className="country-info">
@@ -19,8 +38,18 @@ const CountryInfo = ({ country }) => {
             alt={`Flag of ${country.name.common}`}
             width="300"
             />
-
-        </div>
+            <h2>Weather in {country.capital[0]}</h2>
+            {weather && (
+            <div>
+            <p>Temperature: {weather.main.temp} Celsius</p>
+            <img
+            src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} 
+            alt={weather.weather[0].description} 
+            />
+             <p>Wind: {weather.wind.speed} m/s</p>
+            </div>
+            )}
+            </div>
     )
 
 }
